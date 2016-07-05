947143226de5d07ccf73d9acb4c354aaa890574a
Grant <ghwilley@gmail.com>
2016-07-05 18:27:36 -0500

    jqueried all nav links

--- app.js
+++ app.js
@@ -1,27 +1,42 @@
 $(document).ready(function() {
 	$('.sum_link').click(function(e) {
 		e.preventDefault();
+		$('.nav').css('color', 'black');
+		$(this).css('color', 'red');
+		$('.content').css('display', 'none');
 		$('.summary').toggle();
 	});
 
 
 	$('.skill_link').click(function(e) {
 		e.preventDefault();
+		$('.nav').css('color', 'black');
+		$(this).css('color', 'red');
+		$('.content').css('display', 'none');
 		$('.skills').toggle();
 	});
 
 	$('.employ_link').click(function(e) {
 		e.preventDefault();
+		$('.nav').css('color', 'black');
+		$(this).css('color', 'red');
+		$('.content').css('display', 'none');
 		$('.employment').toggle();
 	});
 
 	$('.edu_link').click(function(e) {
 		e.preventDefault();
+		$('.nav').css('color', 'black');
+		$(this).css('color', 'red');
+		$('.content').css('display', 'none');
 		$('.education').toggle();
 	});
 
 	$('.ach_link').click(function(e) {
 		e.preventDefault();
+		$('.nav').css('color', 'black');
+		$(this).css('color', 'red');
+		$('.content').css('display', 'none');
 		$('.achievments').toggle();
 	});
 
