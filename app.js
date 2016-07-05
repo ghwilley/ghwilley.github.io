e3d69a9d984169df4dd1b0b43135037c35425809
Grant <ghwilley@gmail.com>
2016-07-05 18:20:15 -0500

    added some more styling

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
-	$('.sum_link').click(function() {
-		$.preventDefault();
-		$('.summary').css('color', 'red');
-	})
+	$('.sum_link').click(function(e) {
+		e.preventDefault();
+		$('.summary').toggle();
+	});
+
+
+	$('.skill_link').click(function(e) {
+		e.preventDefault();
+		$('.skills').toggle();
+	});
+
+	$('.employ_link').click(function(e) {
+		e.preventDefault();
+		$('.employment').toggle();
+	});
+
+	$('.edu_link').click(function(e) {
+		e.preventDefault();
+		$('.education').toggle();
+	});
+
+	$('.ach_link').click(function(e) {
+		e.preventDefault();
+		$('.achievments').toggle();
+	});
 
 });
