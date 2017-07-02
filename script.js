21a234d60b8c1adca7a63a146837b8e5a90fa3c2
Grant <ghwilley@gmail.com>
2017-07-02 15:28:41 -0500

    removed photos for now. Instagram is a pain

--- script.js
+++ script.js
@@ -23,12 +23,6 @@ $(document).ready(function(){
         $('.pricing > div > div:nth-of-type(3)').insertAfter($('.pricing > div > div:nth-of-type(1)'));
     }
 
-    // var feed = new Instafeed({
-    //     get: 'tagged',
-    //     tagName: 'awesome',
-    //     clientId: 'YOUR_CLIENT_ID'
-    // });
-    // feed.run();
 
 
 });
